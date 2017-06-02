abe0af1b652cefce2d885f66cd36ed2b88aeb069
tomrhernandez <tomrhernandez@gmail.com>
2017-06-02 17:20:48 +0000

    recommit change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
   config.serve_static_assets = true
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
   config.assets.compile = true
+  
 
   # Code is not reloaded between requests.
   config.cache_classes = true
